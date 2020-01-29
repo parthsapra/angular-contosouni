1b81b8759537679d2863dad1f712b0a806c3ce4c
parthsapra <parthsapra91@gmail.com>
2020-01-29 09:38:09 +0530

    2 files modified

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -8,9 +8,14 @@ import {Router} from '@angular/router';
 })
 export class StudentComponent implements OnInit {
 
- constructor(private router:Router){
+ constructor(private router:Router){}
 
-}
+ students=[
+   {id:1,Name:"bob",emailId:"bob@gmail.com",password:"123"},
+   {id:2,Name:"alice",emailId:"alice@gmail.com",password:"456"},
+   {id:3,Name:"alex",emailId:"alex@gmail.com",password:"789"},
+   {id:4,Name:"tom",emailId:"tom@gmail.com",password:"741"},
+ ]
 
   ngOnInit() {
   }
