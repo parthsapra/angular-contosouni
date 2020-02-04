add52d37d05da0f407b3ec4d0851bc2ff8421740
parthsapra <parthsapra91@gmail.com>
2020-02-04 14:31:31 +0530

    1 file modified

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { StudentData } from "../../studentData.model";.
 
 @Component({
   selector: "app-signin",
@@ -8,15 +9,16 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
   styleUrls: ["./signin.component.css"]
 })
 export class SigninComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder,private router:Router) {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
   studentFormGroup: FormGroup;
 
-students=[
-   {id:1,Name:"bob",emailId:"bob@gmail.com",password:"123"},
-   {id:2,Name:"alice",emailId:"alice@gmail.com",password:"456"},
-   {id:3,Name:"alex",emailId:"alex@gmail.com",password:"789"},
-   {id:4,Name:"tom",emailId:"tom@gmail.com",password:"741"},
- ]
+  students = StudentData;
+  // [
+  //   { id: 1, Name: "bob", emailId: "bob@gmail.com", password: "123" },
+  //   { id: 2, Name: "alice", emailId: "alice@gmail.com", password: "456" },
+  //   { id: 3, Name: "alex", emailId: "alex@gmail.com", password: "789" },
+  //   { id: 4, Name: "tom", emailId: "tom@gmail.com", password: "741" }
+  // ];
 
   ngOnInit() {
     this.studentFormGroup = this.formBuilder.group({
@@ -26,15 +28,16 @@ students=[
   }
 
   signIn() {
-//     var check:Boolean=false;
-//     for(var i=0;i<this.students.length;i++){
-//     if(this.studentFormGroup.value.email==this.students[i].emailId && this.studentFormGroup.value.password==this.students[i].password)
-//     {
-      this.router.navigateByUrl('/student');
-//       check=true;
-// break;
-//     }
-//   }
-//   if(check==false){alert("Invalid Email or Password!!");}
-}
+        var check:Boolean=false;
+        for(var i=0;i<this.students.length;i++){
+        if(this.studentFormGroup.controls.email.value==this.students[i].emailId && this.studentFormGroup.controls.password.value==this.students[i].password)
+        {
+    // this.router.navigateByUrl("/student");
+    console.log("passpass");
+          check=true;
+    break;
+        }
+      }
+      // if(check==false){alert("Invalid Email or Password!!");}
+  }
 }
